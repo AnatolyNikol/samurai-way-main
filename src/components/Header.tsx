41a45e2e69308e2a283cb421b9e90f1d30051193
AnatolyNikol <fanka4ectba@mail.ru>
2023-06-24 02:14:15 +0500

    Add Header, Navbar, Profile

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className='header'>
+            <img src="https://static.vecteezy.com/system/resources/previews/007/636/859/original/community-logo-design-free-vector.jpg" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Profile() {
+    return (
+        <div className='content'>
+            <div className='imgContainer'></div>
+            <div className='avatarContainer'>ava + description</div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
