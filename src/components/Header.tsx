bef89055a4f67beef6ee9330ba2c6dfcc84dcb4b
AnatolyNikol <fanka4ectba@mail.ru>
2023-06-24 03:11:08 +0500

    add css-modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Header.module.css';
 
 function Header() {
     return (
-        <header className='header'>
+        <header className={style.header}>
             <img src="https://static.vecteezy.com/system/resources/previews/007/636/859/original/community-logo-design-free-vector.jpg" alt=""/>
         </header>
     )
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,18 +1,19 @@
 import React from "react";
+import style from './Profile.module.css';
 
 function Profile() {
     return (
-        <div className='content'>
-            <div className='imgContainer'></div>
-            <div className='avatarContainer'>ava + description</div>
+        <div className={style.content}>
+            <div className={style.imgContainer}></div>
+            <div className={style.avatarContainer}>ava + description</div>
             <div>
                 My posts
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>Post 1</div>
-                    <div>Post 2</div>
+                <div className={style.posts}>
+                    <div className={style.item}>Post 1</div>
+                    <div className={style.item}>Post 2</div>
                 </div>
             </div>
         </div>
