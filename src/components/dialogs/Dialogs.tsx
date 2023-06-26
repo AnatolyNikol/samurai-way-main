e5bfa1d1c0c1ab98b091dd9938eb1dba5f485706
AnatolyNikol <fanka4ectba@mail.ru>
2023-06-27 03:15:16 +0500

    25 - map

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -3,27 +3,52 @@ import style from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
 function Dialogs() {
+
+    let dialogs = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valery'}
+    ]
+
+    let messages = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your it-kamasutra?'},
+        {id: 3, message: 'Yo'}
+    ]
+
+    let dialogsElements =  dialogs.map(dialog =>
+            <DialogItem id={dialog.id} name={dialog.name}/>
+        )
+    let messagesElements = messages.map(message =>
+        <Message id={message.id} message={message.message}/>
+    )
+
     return (
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
-                <DialogItem name="Dimych" id="1"/>
-                <DialogItem name="Andrey" id="2"/>
-                <DialogItem name="Sveta" id="3"/>
-                <DialogItem name="Sasha" id="4"/>
-                <DialogItem name="Viktor" id="5"/>
-                <DialogItem name="Valery" id="6"/>
+                {dialogsElements}
+                {/*<DialogItem name="Dimych" id="1"/>*/}
+                {/*<DialogItem name="Andrey" id="2"/>*/}
+                {/*<DialogItem name="Sveta" id="3"/>*/}
+                {/*<DialogItem name="Sasha" id="4"/>*/}
+                {/*<DialogItem name="Viktor" id="5"/>*/}
+                {/*<DialogItem name="Valery" id="6"/>*/}
             </div>
             <div className={style.messages}>
-                <Message message="Hi"/>
-                <Message message="How is your it-kamasutra?"/>
-                <Message message="Yo"/>
+                {messagesElements}
+                {/*<Message message="Hi"/>*/}
+                {/*<Message message="How is your it-kamasutra?"/>*/}
+                {/*<Message message="Yo"/>*/}
             </div>
         </div>
     )
 }
 
 type DialogItemPropsType = {
-    id: string
+    id: number
     name: string
 }
 
@@ -39,6 +64,7 @@ function DialogItem(props: DialogItemPropsType) {
 }
 
 type MessagePropsType = {
+    id: number
     message: string
 }
 
--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -4,6 +4,15 @@ import Post from "./post/Post";
 
 
 function MyPosts() {
+
+    let postsData = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 15},
+        {id: 1, message: 'It\'s my first post', likesCount: 20}
+    ]
+
+    let postsElements = postsData.map(post =>
+        <Post message={post.message} likesCount={post.likesCount}/>)
+
     return (
         <div className={style.postsBlock}>
             <h3>My posts</h3>
@@ -16,8 +25,7 @@ function MyPosts() {
                 </div>
             </div>
             <div className={style.posts}>
-                <Post message="Hi, how are you?" likesCount={15}/>
-                <Post message="It's my first post" likesCount={20}/>
+                {postsElements}
             </div>
         </div>
     )
