60029a5506ac62da4371f516b78d4e6b1e4b7a48
AnatolyNikol <fanka4ectba@mail.ru>
2023-06-26 23:12:16 +0500

    21 - Designing the Dialogs page

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -3,8 +3,29 @@ import style from './Dialogs.module.css'
 
 function Dialogs() {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={`${style.dialog} ${style.active}`}>
+                    Dimych
+                </div>
+                <div className={style.dialog}>
+                    Andrey
+                </div>
+                <div className={style.dialog}>
+                    Sveta
+                </div>
+                <div className={style.dialog}>
+                    Sasha
+                </div>
+                <div className={style.dialog}>
+                    Valery
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How is your it-kamasutra?</div>
+                <div className={style.message}>Yo</div>
+            </div>
         </div>
     )
 }
