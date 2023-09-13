924ee928521bb56044b5cbbb812e929d56fc6910
AnatolyNikol <fanka4ectba@mail.ru>
2023-09-14 02:11:27 +0500

    54 - componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -12,14 +12,12 @@ type usersPropsType = {
 }
 
 class Users extends React.Component<usersPropsType, any> {
-    constructor(props: usersPropsType) {
-        super(props);
 
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
-
     }
 
     render() {
